651617629f24630de9e5c042c177233339cd7f1e
OussamaAtifii <oussamaati03@gmail.com>
2024-01-14 13:24:18 +0100

    Add ProductEntity class

---
+++ src/main/java/com/atifi/products/models/entities/ProductEntity.java
@@ -0,0 +1,33 @@
+package com.atifi.products.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Data
+@Table(name = "products")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+}
